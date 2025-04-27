e0e9afe809fe8259526f5410153064ea84b17a21
Nathan Louth <nathanlouth@aol.com>
2025-04-27 12:41:20 +0100

    fix: add libglib2.0-0 to wsl packages

--- scripts/linux/dev-start.sh
+++ scripts/linux/dev-start.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" == "--wsl-packages" ]; then
     sudo apt update -y
-    sudo apt install -y libgtk-3-0 libxss1 libnss3 libgconf-2-4 libasound2 libx11-xcb1 libxtst6 libcanberra-gtk3-0 libnss3 libxrandr2 libgbm1
+    sudo apt install -y libgtk-3-0 libxss1 libnss3 libgconf-2-4 libasound2 libx11-xcb1 libxtst6 libcanberra-gtk3-0 libnss3 libxrandr2 libgbm1 libglib2.0-0
 fi
 
 clear
