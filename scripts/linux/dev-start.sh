74b60dda9e5bcb30e0e2349dc86484ab9af3a025
Nathan Louth <nathanlouth@aol.com>
2025-04-27 12:17:22 +0100

    feat: add option to install packages for running app on WSL (Debian)

--- scripts/linux/dev-start.sh
+++ scripts/linux/dev-start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+if [ "$1" == "--wsl-packages" ]; then
+    sudo apt update -y
+    sudo apt install -y libgtk-3-0 libxss1 libnss3 libgconf-2-4 libasound2 libx11-xcb1 libxtst6 libcanberra-gtk3-0 libnss3 libxrandr2 libgbm1
+fi
+
 clear
 npm start
